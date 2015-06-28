6b733a01030d7bebcae2538cdb4404837bed0c4f
J Ho Xray <jho.xray@gmail.com>
2015-06-28 16:56:24 +0200

    added some system related commands

--- app/Main.hs
+++ app/Main.hs
@@ -5,8 +5,11 @@ import System.Environment
 import System.Console.Haskeline
 
 import System.CPUTime
+import Data.Time.Clock
+import System.Info
+import System.Environment
 
-import Control.Monad.IO.Class
+import Control.Monad.IO.Class -- liftIO !!!
 
 prompt = "hs>> "
 
@@ -20,7 +23,20 @@ loop = do
    case minput of
        Nothing -> return ()
        Just ":quit"     -> return ()
-       Just ":time"     -> do liftIO getCPUTime >>= outputStrLn . show >> loop -- <- ok, this took a LONG time to figure out. getCPUTime returns IO Int, which needs to be transformed to String and then lifted to InputT IO String...
+       Just ":ptime"    -> do liftIO getCPUTime >>= outputStrLn . show >> loop -- <- ok, this took a LONG time to figure out. getCPUTime returns IO Int, which needs to be transformed to String and then lifted to InputT IO String...
+       Just ":time"     -> do liftIO getCurrentTime >>= outputStrLn . show >> loop
+       Just ":env"      -> do
+                                env <- liftIO getEnvironment
+                                let tmp1 (k,v) = show k ++ " = " ++ show v
+                                mapM_ outputStrLn (map tmp1 env)
+                                loop
+                                
+       Just ":sysinfo"  -> do 
+                                outputStrLn os
+                                outputStrLn arch
+                                outputStrLn compilerName
+                                outputStrLn $ show compilerVersion
+                                loop
        Just ":help"     -> do outputStrLn commandsHelp >> loop
        Just input       -> do outputStrLn (input ++ ": - unknown command. Try :help") >> loop
 
