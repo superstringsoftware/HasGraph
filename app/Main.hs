80c82edffd3fd4c7abe3ae32ec8dc9e1591aec2a
J Ho Xray <jho.xray@gmail.com>
2015-06-28 16:19:47 +0200

    figured out IO vs InputT tranformation - liftIO!!!

--- app/Main.hs
+++ app/Main.hs
@@ -4,21 +4,27 @@ import System.IO
 import System.Environment
 import System.Console.Haskeline
 
-evalString :: String -> IO String
-evalString expr = case expr of 
-    ":help" -> return "Available commands are:\n:quit - exits the program"
-    ('n':'e':'w':' ':xs) -> return $ "Creating object " ++ xs
-    _ -> return "Unknown command. Try :help"
+import System.CPUTime
+
+import Control.Monad.IO.Class
+
+prompt = "hs>> "
+
+commandsHelp = "Available commands are:\n\
+    \:quit - exits the program\n\
+    \:time - show current time"
 
 loop :: InputT IO ()
 loop = do
-   minput <- getInputLine "hs>> "
+   minput <- getInputLine prompt
    case minput of
        Nothing -> return ()
-       Just ":quit" -> return ()
-       Just ":help" -> do outputStrLn "Available commands are:\n:quit - exits the program" >> loop
-       Just input -> do outputStrLn $ input ++ ": - unknown command. Try :help"
-                        loop
+       Just ":quit"     -> return ()
+       Just ":time"     -> do liftIO getCPUTime >>= outputStrLn . show >> loop -- <- ok, this took a LONG time to figure out. getCPUTime returns IO Int, which needs to be transformed to String and then lifted to InputT IO String...
+       Just ":help"     -> do outputStrLn commandsHelp >> loop
+       Just input       -> do outputStrLn (input ++ ": - unknown command. Try :help") >> loop
+
+                        
 
 
 
